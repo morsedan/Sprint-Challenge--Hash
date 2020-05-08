4b6342e605f1f32f1b20a6b5353b0e52563ffdc9
Dan Morse <djmthedev@gmail.com>
2020-05-08 08:10:16 -0700

    ex2 complete

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,16 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    ticket_dict = {}
+    route = [None] * (len(tickets))
+    for ticket in tickets:
+        if ticket.source == "NONE":
+            route[0] = ticket.destination
+        ticket_dict[ticket.source] = ticket.destination
+
+    for i in range(1, len(tickets)):
+        route[i] = ticket_dict[route[i-1]]
+    print(ticket_dict)
+    print(route)
 
     return route
