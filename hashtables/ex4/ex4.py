b875392aa814f1132f2bab3429771fab6bb6f83b
Dan Morse <djmthedev@gmail.com>
2020-05-08 08:18:15 -0700

    completed ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,14 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-
+    num_dict = {}
+    result = []
+    for num in a:
+        num_dict[num] = None
+    for num in a:
+        if num > 0:
+            if num * -1 in num_dict:
+                result.append(num)
     return result
 
 
