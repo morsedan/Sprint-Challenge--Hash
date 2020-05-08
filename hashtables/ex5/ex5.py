e9878127261612ec9e56966018954bf5709cdeca
Dan Morse <djmthedev@gmail.com>
2020-05-08 08:24:56 -0700

    completed ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -3,6 +3,20 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
+    q_dict = {}
+    result = []
+    # for query in queries:
+    #     q_dict[query] = None
+    for file in files:
+        last = file.split("/")[-1]
+        if last in q_dict:
+            q_dict[last].append(file)
+        else:
+            q_dict[last] = [file]
+    for query in queries:
+        if query in q_dict:
+            for file in q_dict[query]:
+                result.append(file)
 
     return result
 
